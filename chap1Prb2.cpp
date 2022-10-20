d25a00f7d6bb898d4e9dae82b040cf0863639770
AfsanaMimi009 <116268093+AfsanaMimi009@users.noreply.github.com>
2022-10-20 15:49:11 +0600

    Add files via upload

---
+++ chap1Prb2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+//12,32,43,1,54,53,15,64,3,13
+//without function
+
+int main () {
+ int a [10] = {12,32,43,1,54,53,15,64,3,13} ;
+
+ int even = 0,odd=0;
+
+ for ( int i =0; i<10;i++) {
+
+ if ( a[i] % 2 ==0){
+  even++;
+ } else{
+   odd++;
+   }
+ }
+ cout<<odd<<" odd numbers"<<endl;
+ cout<<even<<" even numbers" <<endl;
+
+ return 0;
+
+}
